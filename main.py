6be3c45f45fccdc58abcd74d24e3a0d80e6488f9
vylarrja <vylarrja@student.21-school.ru>
2026-07-06 20:52:10 +0300

    final

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
 from datetime import datetime, date, time
 from Crypto.Cipher import AES
-from application.modules.salary import calculate_salary
-from application.modules.people import get_employees
+from application.salary import calculate_salary
+from application.db.people import get_employees
 
 
 
